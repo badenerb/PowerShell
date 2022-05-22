c401aad4cefcf4c77b95f35485ef67dd99217d7a
Baden <97712507+badenerb@users.noreply.github.com>
2022-05-21 20:57:57 -0500

    Update FileExplorer.ps1

--- FileExplorer.ps1
+++ FileExplorer.ps1
@@ -1,3 +1,8 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
+Add-Type -AssemblyName System.Windows.Forms
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'Varonis Output Files (*.csv)|*.csv'
+}
+
 $null = $FileBrowser.ShowDialog()
