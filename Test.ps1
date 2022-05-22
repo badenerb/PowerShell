67f091cbd3b17a316207262545b26cbfcadf4b85
Baden <97712507+badenerb@users.noreply.github.com>
2022-05-21 20:52:38 -0500

    File Explorer
    https://4sysops.com/archives/how-to-create-an-open-file-folder-dialog-box-with-powershell/

---
+++ Test.ps1
@@ -0,0 +1,3 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
+$null = $FileBrowser.ShowDialog()
